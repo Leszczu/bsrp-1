05a9057a2db29798e7727ff3417a3b2d37686a69
Adrian Leszczynski <bangeusz@gmail.com>
2011-11-06 12:01:17 +0100

    gifs

--- app/models/player.rb
+++ app/models/player.rb
@@ -22,7 +22,7 @@
   # Avatar nie jest wymagany, opcjonalnie
   # validates_attachment_presence :avatar
   validates_attachment_size :avatar, :less_than => 5.megabytes
-  validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png']
+  validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 
   #def validate_old_password
   #  if self.password.nil? || (!self.password_hash.nil? && get_password_hash(self.password) != self.password_hash)
